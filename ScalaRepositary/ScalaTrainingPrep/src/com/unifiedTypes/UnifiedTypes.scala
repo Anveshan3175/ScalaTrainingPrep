e9839d2483ceecdf0dfce2f06426db70735a18ce
M1019703 <M1019703@A2ML16815.mindtree.com>
2016-04-15 19:51:13 +0530

    Signed-off-by: M1019703 <M1019703@A2ML16815.mindtree.com>

--- ScalaRepositary/ScalaTrainingPrep/src/com/unifiedTypes/UnifiedTypes.scala
+++ ScalaRepositary/ScalaTrainingPrep/src/com/unifiedTypes/UnifiedTypes.scala
@@ -1,5 +1,24 @@
 package com.unifiedTypes
 
-object UnifiedTypes {
+// UnifiedTypes is singleton object
+object UnifiedTypes extends App {
+
+    val set = new scala.collection.mutable.LinkedHashSet[Any]
   
+    /* The elements are added to the set
+     * The elements have to conform to the declared set element type Any
+     */
+    set += "This is String" // += internally calls add() method ..Adds a string
+    set += 456 // adds a number
+    set += 'c' // adds a character
+    set += true // adds a boolean
+    set += main _ //add main function
+  
+    val iter: Iterator[Any] = set.iterator
+  
+    while (iter.hasNext) {
+      //println(iter.next.toString())
+      println(iter.next)
+    }
+
 }
