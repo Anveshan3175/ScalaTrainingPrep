a62583fc433780c1c0f3ca240534dc724ce7d3c0
Anveshan <akunduru4@gmail.com>
2016-04-24 16:03:57 +0530

    Signed-off-by: Anveshan <akunduru4@gmail.com>

---
+++ ScalaRepositary/ScalaTrainingPrep/src/com/anonymousFuncs/TestAnonyFunctions.scala
@@ -0,0 +1,39 @@
+package com.anonymousFuncs
+
+/*object TestAnonyFunctions extends App{
+  def add(a:Int,b:Int):Int = a + b
+}*/
+
+object TestAnonyFunctions {
+  
+ 
+  type twoParamFunc = (Int,Int) => Int
+  type oneParamFunc = (Int) => Int
+  def incr(a:Int):Int = a + 1
+  
+  def main(args:Array[String]){
+    println("Testing anonymous functions");
+    
+    // Defining and using a function - 1
+    def add(a:Int,b:Int):Int = a + b  // can define new function inside a function
+    println("Calling addition of numbers "+add(3,4))
+    
+    // Defining and using a function - 2
+    val incr = (x:Int) => x + 1
+    println("Calling incr for 3 :"+incr(3))
+    
+    //val addFunction = (a:Int,b:Int) =>  a + b  will compile
+    //val sum = (x:Int,y:Int):Int => x + y  will not compile  ?
+    
+    // Defining and using a function - 3..Using Type in the function definition
+    def manipulate(a:Int,b:Int,f:twoParamFunc):String = { "The result is "+f(a,b)}
+    val sumFun = manipulate(3,4,add)
+    println(sumFun)
+    def diff(a:Int,b:Int):Int = {a - b}
+    val diffFun = manipulate(3,4,diff)
+    println(diffFun)
+    
+  }
+  
+ 
+}
